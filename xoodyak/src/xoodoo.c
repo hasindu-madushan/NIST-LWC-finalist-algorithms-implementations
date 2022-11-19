ccf9f6ee26e8af7ac79834e388b3dcd141d85636
Hasindu Madushan <hasindumadushan325@gmail.com>
2022-11-19 17:44:35 +0530

    xoodyak: Add non-linear layer to the xoodoo permutation

--- xoodyak/src/xoodoo.c
+++ xoodyak/src/xoodoo.c
@@ -29,6 +29,10 @@ void add_planes(uint8_t *output, uint8_t *plane1, uint8_t *plane2);
 void rotate_plane(uint8_t *output, uint8_t *plane, uint8_t x, uint8_t y);
 void shift_planes(uint8_t *state, uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
 
+void inverse_product_add(uint8_t *state, uint8_t output_index, uint8_t plane1_index, uint8_t plane2_index);
+void invert_plane(uint8_t *output, uint8_t *plane);
+void and_planes(uint8_t *output, uint8_t *plane1, uint8_t *plane2);
+
 
 void xoodoo(uint8_t *state)
 {
@@ -78,6 +82,45 @@ void shift_planes_west(uint8_t *state)
     shift_planes(state, 1, 0, 0, 11);
 }
 
+void add_round_constants(uint8_t *state, int8_t round)
+{
+    ((uint32_t*)state)[0] ^= round_constants[MAXROUNDS + round - 1];
+}
+
+void non_linear_layer(uint8_t *state)
+{
+    inverse_product_add(state, 0, 1, 2); 
+    inverse_product_add(state, 1, 2, 0); 
+    inverse_product_add(state, 2, 0, 1); 
+}
+
+/* Bx = Ax1'.Ax2 
+ * Ax = Ax + Bx */
+void inverse_product_add(uint8_t *state, uint8_t output_index, uint8_t plane1_index, uint8_t plane2_index)
+{
+    uint8_t temp[PLANE_SIZE];
+    invert_plane(temp, GET_PLANE(state, plane1_index));
+    and_planes(temp, temp, GET_PLANE(state, plane2_index));
+    add_planes(GET_PLANE(state, output_index), GET_PLANE(state, output_index), temp);
+}
+
+void invert_plane(uint8_t *output, uint8_t *plane)
+{
+    for (uint8_t i = 0; i < LANES_PER_PLANE; i++)
+	((uint32_t*)output)[i] = ((uint32_t*)plane)[i];
+}
+
+void and_planes(uint8_t *output, uint8_t *plane1, uint8_t *plane2)
+{
+    for (uint8_t i = 0; i < LANES_PER_PLANE; i++)
+	((uint32_t*)output)[i] = ((uint32_t*)plane1)[i] ^ ((uint32_t*)plane2)[i];
+}
+
+void shift_planes_east(uint8_t *state)
+{
+    shift_planes(state, 0, 1, 2, 8);
+}
+
 void shift_planes(uint8_t *state, uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2)
 {
     uint8_t temp[PLANE_SIZE];
@@ -88,17 +131,4 @@ void shift_planes(uint8_t *state, uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2
     /* A2 = A2 <<< (x2, y2) */
     rotate_plane(temp, GET_PLANE(state, 2), x2, y2);
     memcpy(GET_PLANE(state, 2), temp, PLANE_SIZE);
-}
-
-void add_round_constants(uint8_t *state, int8_t round)
-{
-    ((uint32_t*)state)[0] ^= round_constants[MAXROUNDS + round - 1];
-}
-
-void non_linear_layer(uint8_t *state)
-{
-}
-
-void shift_planes_east(uint8_t *state)
-{
 }
